30572af929458bcdd6d6e26d004847ec5658c5a7
shalinichauhanpurohit <shalinichauhanpurohit@yahoo.com>
2024-06-19 13:18:43 +0000

    Updating Project

--- src/test/java/PizzaHutTestNGimplementation/pizzaHutTest.java
+++ src/test/java/PizzaHutTestNGimplementation/pizzaHutTest.java
@@ -15,6 +15,7 @@ import com.aventstack.extentreports.ExtentTest;
 public class pizzaHutTest extends BaseTest{
 	
 	ExtentTest test;
+	
 	   
 	@BeforeSuite
 	@Parameters("url")
@@ -28,11 +29,10 @@ public class pizzaHutTest extends BaseTest{
         openBrowser();
                         
         // Open application URL
-        System.out.println("Current URL is "+ url);
         openApplicationURL(url);
         
         // Delete cookies
-         deleteCookies();
+        deleteCookies();
         
         // Set page load timeout
         setPageLoadTimeout(5000);
@@ -50,30 +50,25 @@ public class pizzaHutTest extends BaseTest{
 		setPageLoadTimeout(5000);
 
 		//close the location blackbox
-	      	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 seconds timeout
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 seconds timeout
 		   	WebElement blackBox1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-synth='close-offer-collection']")));
 		   	blackBox1.click();
 		   
 		
 		//Set the user delivery location as Lulu Mall, Bangalore
 			WebElement locationSetup = driver.findElement(By.xpath("//input[@data-synth='input--google-places']"));
-			locationSetup.sendKeys("Lulu Mall ");
-			
-			WebElement locationSel = driver.findElement(By.xpath("(//div[@class='typography-4'])[1]"));
-			locationSel.click();
-			//WebElement locationSet = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class=text-left pl-15 py-10 border-t border-l-0 border-r-0 border-b-0 border-grey-lightest border-solid suggestion-item']")));
-		    //locationSet.click();
-			WebElement finalLocation = driver.findElement(By.xpath("(//p[@data-synth='hut-option-name-distance'])[3]"));
-			finalLocation.click();
-			
+			locationSetup.sendKeys("Lulu Mall Banglore");
+			WebElement flexBox = driver.findElement(By.xpath("//div[@class='pt-5 border-t overflow-scrolling-touch']"));
+			flexBox.isDisplayed();
+			WebElement locationSet = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='text-left pl-15 py-10 border-t border-l-0 border-r-0 border-b-0 border-grey-lightest border-solid suggestion-item'][1]")));
+		    locationSet.click();
+		    	
 			test.pass("Location set");
-			System.out.println("Inside Set Location Pass Method.");
 		
 		}	
 		catch(Exception e)
 		{
 			test.fail("Location is not set"+e.getMessage())	;
-			System.out.println("Inside Set Location Fail Method.");
 		}
 	}
 	
@@ -85,12 +80,11 @@ public class pizzaHutTest extends BaseTest{
 		//The user is now on the Deals page. Validate that the URL has text as ‘deals’
 			String currentUrl = driver.getCurrentUrl();
 			assert currentUrl.contains("deals");
+		 
 			test.pass("URL contain the word deals.");
-			System.out.println("Inside 2nd Pass Method.");
 		}
 		catch(AssertionError e) {
 			test.fail("URL does not contain the work deals"+e.getMessage());
-			System.out.println("Inside 2nd fail Method.");
 		}
 		
 	}
@@ -100,27 +94,22 @@ public class pizzaHutTest extends BaseTest{
 	{
 		try {
 			
-		//Navigate to Sides Menu	
-			WebElement sideMenu = driver.findElement(By.xpath("//a[@href='/order/sides/']"));
+		//Navigate to Sides Menu
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10 seconds timeout
+			WebElement sideMenu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-synth='link--sides--side']")));
 			sideMenu.click();
+			
 			test.pass("The user is on the Sides page");
 		
 		//Adding Item to the basket which is below Rs.200	
 			WebElement item = driver.findElement(By.xpath("//span[@class='w-auto ml-3']"));
-			item.click();
-		
-			WebElement addToBasket = driver.findElement(By.linkText("Add"));
-			addToBasket.click();
+			item.click();		
 			test.pass("Item below 200 added to the basket.");
-			WebElement checkOut = driver.findElement(By.xpath("//a[@href=https://www.pizzahut.co.in/order/checkout/]"));
-			assert checkOut.getText().isEmpty();
-			System.out.println("Inside 3rd pass Method.");
 		
 		}
 		catch(Exception e)
 		{
 			test.fail("Items not added to the basket"+e.getMessage());
-			System.out.println("Inside 3rd fail Method.");
 		}
 	}
 	
@@ -130,26 +119,27 @@ public class pizzaHutTest extends BaseTest{
 		try {
 			
 		//Navigate to page DRINKS
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // 10 seconds timeout
 			WebElement addDrink = driver.findElement(By.linkText("Drinks"));
 			addDrink.click();
 			test.pass("The user is on the Drinks page.");
 		
 		//Add 2 drinks 
-			WebElement item1 = driver.findElement(By.xpath("//button[@dataset.synth=button--pepsi-600ml--one-tap]"));
-			item1.click();
-			WebElement item2 = driver.findElement(By.xpath("//button[@dataset.synth=button--7-up-600ml--one-tap]"));
+			WebElement item1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-synth='link--pepsi-600ml']")));
+			item1.click();			
+			WebElement item1Add = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-synth='button--add-to-basket']")));
+			item1Add.click();	
+			WebElement item2 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@data-synth='link--pepsi-black-250ml']")));
 			item2.click();
+			WebElement item2Add = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@data-synth='button--add-to-basket']")));
+			item2Add.click();
 			
-			WebElement addToBasket = driver.findElement(By.linkText("Add"));
-			addToBasket.click();
 			test.pass("Two drinks added to the basket");
-			System.out.println("Inside 4th pass Method.");
 	
 		}
 		catch(Exception e)
 		{
 			test.fail("Items not added to the basket"+e.getMessage());
-			System.out.println("Inside 4th fail Method.");
 		}
 	}
 		
@@ -158,7 +148,8 @@ public class pizzaHutTest extends BaseTest{
 		
 		try {
 		//Checkout the order
-			WebElement checkOut = driver.findElement(By.xpath("//a[@href=https://www.pizzahut.co.in/order/checkout/]"));
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20)); // 10 seconds timeout
+			WebElement checkOut = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Checkout']")));
 			checkOut.click();
 			test.pass("The user is on the checkout page.");
 		
@@ -166,11 +157,6 @@ public class pizzaHutTest extends BaseTest{
 			WebElement onlinePay = driver.findElement(By.id("payment-method--razorpay"));
 			assert onlinePay.isSelected();
 			test.pass("Online Payment selected by default");
-		
-		//Change to the cash payment
-			WebElement cashPay = driver.findElement(By.id("payment-method--cash"));
-			cashPay.click();
-			test.pass("Payment changed to Cash.");
 		
 		//Validate the "I agree" option is selected
 			WebElement agreePay = driver.findElement(By.id("marketingOptIn"));
@@ -182,35 +168,42 @@ public class pizzaHutTest extends BaseTest{
 			name.sendKeys("Shalini");
 			WebElement email = driver.findElement(By.id("checkout__email"));
 			email.sendKeys("shalinichauhanpurohit@yahoo.com");
-			WebElement address = driver.findElement(By.id("checkout__phone"));
-			address.sendKeys("9687482564");
+			WebElement phoneNo = driver.findElement(By.id("checkout__phone"));
+			phoneNo.sendKeys("9687482564");
+			WebElement address = driver.findElement(By.id("checkout__deliveryAddress.interior"));
+			address.sendKeys("Gandhinagar");
 			test.pass("Details are added");
 		
+			
+		//Change to the cash payment
+            WebElement cashPay = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@data-synth='payment-method--cash-label']")));
+			cashPay.click();
+			test.pass("Payment changed to Cash.");
+			
 		//Click on the Apply Gift Card link. A pop up should appear. Click on the Voucher
-			WebElement giftCard = driver.findElement(By.linkText("Apply Gift Card"));
+			WebElement giftCard = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Apply Gift Card']")));
 			giftCard.click();
-			WebElement coupon = driver.findElement(By.linkText("Coupon"));
+			WebElement coupon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Coupon']")));
 			coupon.click();
 		
 		//Give the Voucher code as 12345 and submit
-			WebElement couponCode = driver.findElement(By.xpath("//input[@name=voucherId]"));
+			WebElement couponCode = driver.findElement(By.xpath("//input[@name='voucherId']"));
 			couponCode.sendKeys("12345");
-			WebElement applyButton = driver.findElement(By.xpath("//button[@type=submit]"));
+			WebElement applyButton = driver.findElement(By.xpath("//button[@type='submit']"));
 			applyButton.click();
 		
 		//Validate if an error is coming that the number is incorrect
-			WebElement errorMsg = driver.findElement(By.linkText("Sorry, we don’t currently support that coupon code."));
+			WebElement errorMsg = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Sorry, we don’t currently support that coupon code.']")));
 			assert errorMsg.isDisplayed();
 		
 		//Close the voucher pop up
-			WebElement closePopup = driver.findElement(By.linkText("Cancel"));
+			WebElement closePopup = driver.findElement(By.xpath("//button[@class='icon-remove-3 absolute top-0 right-0 mr-20 mt-20']"));
 			closePopup.click();
-			System.out.println("Inside 5th pass Method.");
+			
 		}
 		catch(Exception e)
 		{
 			test.fail("Process Failed"+ e.getMessage());
-			System.out.println("Inside 5th fail Method.");
 		}
 	}
 	
@@ -218,7 +211,6 @@ public class pizzaHutTest extends BaseTest{
 	    public void processClose() {
 	        driver.quit();
 	        test.pass("Driver session quit");
-	        System.out.println("Inside Final Method.");
 	        extent.flush();
 }
 }
